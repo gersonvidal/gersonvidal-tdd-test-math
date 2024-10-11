dad076221aa6c628201b9fcc3896e7ccb5e40de3
gersonvidal <gtotrac@gmail.com>
2024-10-11 15:54:19 -0600

    cover compute test case when input is 0

--- src/app/compute/compute.spec.ts
+++ src/app/compute/compute.spec.ts
@@ -3,11 +3,19 @@ import { compute } from './compute';
 describe('compute test suite', () => {
   it('Should return 0 if input is negative', () => {
     const result = compute(-1);
+   
     expect(result).toBe(0);
   });
 
   it('Should increment the input if it is positive', () => {
     const result = compute(1);
+   
     expect(result).toBe(2);
-  })
-})
+  });
+
+  it('Should return 0 if the input is 0', () => {
+    const result = compute(0);
+   
+    expect(result).toBe(0);
+  });
+});

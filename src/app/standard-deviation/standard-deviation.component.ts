97e1ef01075ad93dd760e81244f4c9a42ece8d03
gersonvidal <gtotrac@gmail.com>
2024-10-11 14:29:05 -0600

    added tests for standard deviation

---
+++ src/app/standard-deviation/standard-deviation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-standard-deviation',
+  standalone: true,
+  imports: [],
+  templateUrl: './standard-deviation.component.html',
+  styleUrl: './standard-deviation.component.css'
+})
+export class StandardDeviationComponent {
+
+}

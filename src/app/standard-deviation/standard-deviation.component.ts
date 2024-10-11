15f0c110a672def485182e9220cbdf8d8aafb44f
gersonvidal <gtotrac@gmail.com>
2024-10-11 14:46:43 -0600

    add standard_deviation branch #15

--- src/app/standard-deviation/standard-deviation.component.ts
+++ src/app/standard-deviation/standard-deviation.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { calculateMean } from '../mean/mean.component';
 
-@Component({
-  selector: 'app-standard-deviation',
-  standalone: true,
-  imports: [],
-  templateUrl: './standard-deviation.component.html',
-  styleUrl: './standard-deviation.component.css'
-})
-export class StandardDeviationComponent {
+export function calculateStandardDeviation(array: number[]): number {
+  if (array.length === 0) {
+    throw new Error('The array is empty.');
+  }
 
+  const mean = calculateMean(array);
+
+  const sumOfSquaredDifferences = array.reduce((accumulator, currentValue) => {
+    const difference = currentValue - mean;
+    return accumulator + Math.pow(difference, 2);
+  }, 0);
+
+  const variance = sumOfSquaredDifferences / (array.length - 1);
+
+  const standardDeviation = Math.sqrt(variance);
+
+  return parseFloat(standardDeviation.toFixed(2));
 }

e0792537296cbb10aac6a567245f2424bef87bfb
gersonvidal <gtotrac@gmail.com>
2024-11-04 10:30:44 -0600

    finish main menu #26

--- src/app/mean/mean.component.ts
+++ src/app/mean/mean.component.ts
@@ -1,10 +1,25 @@
-export function calculateMean(array: number[]) {
-  if (array.length === 0) {
-    throw new Error("The array is empty.");
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mean',
+  templateUrl: './mean.component.html',
+  styleUrls: ['./mean.component.css'],
+})
+export class MeanComponent implements OnInit {
+  ngOnInit(): void {
   }
+  
+  static calculateMean(array: number[]) {
+    if (array.length === 0) {
+      throw new Error('The array is empty.');
+    }
 
-  const sum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-  const mean = sum / array.length;
+    const sum = array.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    const mean = sum / array.length;
 
-  return parseFloat(mean.toFixed(2));
+    return parseFloat(mean.toFixed(2));
+  }
 }
--- src/app/standard-deviation/standard-deviation.component.ts
+++ src/app/standard-deviation/standard-deviation.component.ts
@@ -1,20 +1,33 @@
-import { calculateMean } from '../mean/mean.component';
+import { Component, OnInit } from '@angular/core';
+import { MeanComponent } from '../mean/mean.component';
 
-export function calculateStandardDeviation(array: number[]): number {
-  if (array.length === 0) {
-    throw new Error('The array is empty.');
+@Component({
+  selector: 'app-standard-deviation',
+  templateUrl: './standard-deviation.component.html',
+  styleUrls: ['./standard-deviation.component.css'],
+})
+export class StandardDeviationComponent implements OnInit {
+  ngOnInit(): void {}
+
+  static calculateStandardDeviation(array: number[]): number {
+    if (array.length === 0) {
+      throw new Error('The array is empty.');
+    }
+
+    const mean = MeanComponent.calculateMean(array);
+
+    const sumOfSquaredDifferences = array.reduce(
+      (accumulator, currentValue) => {
+        const difference = currentValue - mean;
+        return accumulator + Math.pow(difference, 2);
+      },
+      0
+    );
+
+    const variance = sumOfSquaredDifferences / (array.length - 1);
+
+    const standardDeviation = Math.sqrt(variance);
+
+    return parseFloat(standardDeviation.toFixed(2));
   }
-
-  const mean = calculateMean(array);
-
-  const sumOfSquaredDifferences = array.reduce((accumulator, currentValue) => {
-    const difference = currentValue - mean;
-    return accumulator + Math.pow(difference, 2);
-  }, 0);
-
-  const variance = sumOfSquaredDifferences / (array.length - 1);
-
-  const standardDeviation = Math.sqrt(variance);
-
-  return parseFloat(standardDeviation.toFixed(2));
 }
